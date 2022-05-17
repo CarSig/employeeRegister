65c3331460dfa32d7c7aa24c97880f22d110eacc
Lovro Boric <lovro.boric@gmail.com>
2022-05-18 01:54:46 +0200

    heaeder and dashboard - html and hooks

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import Login from "./Login";
+import UserList from "../components/UserList";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const navigate = useNavigate();
+
+  const addNewUser = () => {
+    window.location.href = "/register";
+  };
+
+  return (
+    <div>
+      {/*TODO: remove ! when authorization complete*/}
+      {!localStorage.getItem("username") ? (
+        <div>
+          <h3 variant="h3" color="textSecondary">
+            Dashboard
+          </h3>
+
+          <div className="buttons">
+            <button onClick={addNewUser}>Add New User</button>
+          </div>
+
+          <UserList></UserList>
+        </div>
+      ) : (
+        <Login></Login>
+      )}
+    </div>
+  );
 };
 
 export default Dashboard;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [signed, setSigned] = useState(localStorage.getItem("username"));
+
+  const signOut = () => {
+    localStorage.removeItem("username");
+    localStorage.removeItem("token");
+    window.location.href = "/login";
+  };
+
+  const gotoDash = () => {
+    window.location.href = "/dashboard";
+  };
+  return (
+    <div className="header">
+      {" "}
+      <h4>Employee register App</h4>
+      {
+        //checks if signed with help of local storage, if values are not there redirects
+      }
+      {signed && (
+        <div className="buttonsHeader">
+          <button onClick={gotoDash}>Go to Dashboard</button>
+          <button color="default" disableElevation onClick={signOut}>
+            Sign out
+          </button>
+          <p color="textSecondary">hello {signed}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Header;
